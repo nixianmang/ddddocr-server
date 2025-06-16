e24178438950b0c93223581abd9bb7bb39b0393a
nixianmang <nixianmang1@gmail.com>
2025-06-16 06:42:31 +0000

    start

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
-// const { DdddOcr } = require('ddddocr-node');
 import { DdddOcr } from 'ddddocr-node'
 import express from 'express'
 import cors from 'cors'
 import multer from 'multer'
 const upload = multer({ dest: 'uploads/' })
 
-console.log(multer);
-
-
 const app = express()
-
 app.use(cors())
 app.use(express.json())
 
 const ddddOcr = new DdddOcr();
 
-const result = await ddddOcr.classification('image.png');
-console.log(result);
+// const result = await ddddOcr.classification('image.png');
+// console.log(result);
 
 app.get('/', (_, res) => {
     res.send('hello')
